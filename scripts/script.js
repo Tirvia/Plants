61e8b7abd4782244338a06491e4eb840eaa34752
Hanna <fedoska_ann@mail.ru>
2023-02-05 16:39:59 +0300

    feat: add focus for buttons

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,30 @@ menuBtn.addEventListener('click', function(){
 	menu.classList.toggle('active');
     menuBtn.classList.toggle('active');
 })
+
+var buttons1 = document.getElementById("Gardens");
+var buttons2 = document.getElementById("Lawn");
+var buttons3 = document.getElementById("Planting");
+var but1 = document.getElementById("gardens1");
+var but5 = document.getElementById("gardens2");
+var but2 = document.getElementById("planting1");
+var but3 = document.getElementById("lawn");
+var but4 = document.getElementById("planting2");
+var but6 = document.getElementById("planting3");
+
+
+buttons1.addEventListener('click', function(){
+    buttons1.classList.toggle('active');
+	  but1.classList.toggle('active');
+    but5.classList.toggle('active');
+});
+buttons2.addEventListener('click', function(){
+  buttons2.classList.toggle('active');
+  but3.classList.toggle('active');
+});
+buttons3.addEventListener('click', function(){
+  buttons3.classList.toggle('active');
+  but2.classList.toggle('active');
+  but4.classList.toggle('active');
+  but6.classList.toggle('active');
+});
